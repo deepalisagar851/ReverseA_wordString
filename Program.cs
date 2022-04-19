dbff166e6b2ae10228669cf688738e021af359ed
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-04-19 14:12:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str="", reverse = "";
+            int Length=0;
+            Console.WriteLine("Enter a String :");
+            str = Console.ReadLine();
+             Length = str.Length - 1;
+            while (Length >= 0)
+            {
+                reverse = reverse + str[Length];
+                Length--;
+            }
+                Console.WriteLine("Converted String is :{0}" ,reverse);
+                Console.ReadLine();
+            
+
+        }
+    }
+}
+
